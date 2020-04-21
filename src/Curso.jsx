403757e603450a4de4126578941ef684f2b59bd0
sangello <sebastian.angellotti@capgemini.com>
2020-04-21 00:35:00 -0300

    se cambia el nombre del profesor Martin

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -17,7 +17,7 @@ const Curso = () => (
                 </div>
                 </div>
                 <span className="small">Sebastian</span>
-                <span className="small">Martin</span>
+                <span className="small">Martines</span>
             </div>
             </div>
             <div class="s-main-center">
