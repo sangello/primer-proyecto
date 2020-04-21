1aefd830045f542e505b3a3a496998620ee5b5b9
sangello <sebastian.angellotti@capgemini.com>
2020-04-21 00:06:47 -0300

    Se agrega la carpeta Styles, archivo Styles.scss, archivo Curso.jsx, El primer componente

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+/**import './App.css';*/
+import "./Styles/Styles.scss";
+import Curso from './Curso'
 
 function App() {
   return (
+    /**
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit" <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +23,31 @@ function App() {
         </a>
       </header>
     </div>
+     */
+    <>
+    <div>
+      <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" src="tu-imagen.jpg"/>
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Título del banner</p>
+            <p> Subtítulo del banner</p>
+            <a href="#" className="button">Botón del banner</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+    </>
   );
   
   
---
+++ src/Curso.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Curso = () => (
+    <article class="card">
+        <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+            <img src="https://edteam-media.s3.amazonaws.com/courses/original/91e149d0-961a-4594-a8ff-0a625be9cdd2.png" alt="Curso Go"/>
+        </div>
+        <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+            <h3 className="t5 s-mb-2 s-center">
+            Programación orientada a objetos con Go
+            </h3>
+            <div className="s-mb-2 s-main-center">
+            <div className="card__teacher s-cross-center">
+                <div className="card__avatar s-mr-1">
+                <div className="circle img-container">
+                    <img src="https://edteam-media.s3.amazonaws.com/users/avatar/f2db2437-1baf-481d-9f3a-04ba848008a7.jpg" alt="Profesor"/>
+                </div>
+                </div>
+                <span className="small">Sebastian</span>
+            </div>
+            </div>
+            <div class="s-main-center">
+            <a class="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+            </div>
+        </div>
+</article>
+)
+
+export default Curso
