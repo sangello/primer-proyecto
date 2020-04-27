9ad82550e6d6d72c7b4ce678d6e63f7aa506a4eb
sangello <sebastian.angellotti@capgemini.com>
2020-04-27 15:24:04 -0300

    Clase 5.6 - Parámetros de la ruta

--- src/App.js
+++ src/App.js
@@ -2,32 +2,11 @@ import React from 'react';
 /** import logo from './logo.svg'; */
 /**import './App.css';*/
 import "./Styles/Styles.scss";
-import Curso from './Jsx/Curso'
-import ComponentePresentacionales from './Jsx/ComponentePresentacionales'
-import ImprimiendoPropiedades from './Jsx/ImprimiendoPropiedades'
-import ImprimiendoPropiedades1 from './Jsx/ImprimiendoPropiedades1'
-import PropTypesDefaultprops from './Jsx/PropTypesDefaultprops'
-import RecorriendoArrays from './Jsx/RecorriendoArrays';
 import Banner from './Jsx/Banner';
 import Formulario from './Jsx/Formulario';
 import CourseGrid from './Jsx/CourseGrid';
 import Course from './Jsx/Course';
 import { BrowserRouter as Router , Route, Switch} from "react-router-dom"
-
-const Cursos = [
-  { "title" : "Java Desde 0",
-    "image" : "https://edteam-media.s3.amazonaws.com/courses/original/50668a3f-3a6e-4e24-80ff-dd5bea441559.jpg",
-    "price" :  "120 USD"} , 
-  { "title" : "PHP Desde 0",
-    "image" : "https://edteam-media.s3.amazonaws.com/courses/original/5ec26692-8451-4297-a59a-adb1829a90e8.jpg",
-    "price" :  "20 USD"} , 
-  { "title" : "GO Desde 0",
-    "image" : "https://edteam-media.s3.amazonaws.com/courses/original/91e149d0-961a-4594-a8ff-0a625be9cdd2.png",
-    "price" :  "120 USD"} , 
-  { "title" : "SEO Desde 0",
-    "image" : "https://edteam-media.s3.amazonaws.com/courses/original/9828742f-4da6-426e-80c9-e5081cc7f7a9.png",
-    "price" :  "20 USD"} , 
-]
 
 const App = () => (
 
@@ -51,6 +30,8 @@ const App = () => (
            *  para que no renderize el componente CourseGrid porque es un switch y renderiza de arriba
            *  para abajo.
            * 
+           * ----------- Clase 5.6 - Parámetros de la ruta -------- 
+           * propiedad math, toma todo lo que viene de la url
            */}
         <Switch>
           <Route path="/" exact  component={Banner}/>
--- src/Jsx/Course.jsx
+++ src/Jsx/Course.jsx
@@ -1,12 +1,81 @@
-import React from 'react';
+import React from 'react'
 
-const Course = () => (
-    <div className="ed-grid m-grid-3">
-        <h1 className="m-cols-3">Nombre del Curso</h1>
-        <img className="m-cols-1" src="https://edteam-media.s3.amazonaws.com/courses/original/50668a3f-3a6e-4e24-80ff-dd5bea441559.jpg" alt="Imagen del Curso"/>
-        <p className="m-cols-2"> Descripcion del curso</p>
-    </div>
+const Cursos = [
+    { "id":1,
+      "title" : "Java Desde 0",
+      "image" : "https://edteam-media.s3.amazonaws.com/courses/original/50668a3f-3a6e-4e24-80ff-dd5bea441559.jpg",
+      "price" :  "120 USD",
+      "profesor": "juan Alberto"} , 
+    { "id":2,
+      "title" : "PHP Desde 0",
+      "image" : "https://edteam-media.s3.amazonaws.com/courses/original/5ec26692-8451-4297-a59a-adb1829a90e8.jpg",
+      "price" :  "20 USD",
+      "profesor": "Maxi Muñiz"} ,
+    { "id":3,
+      "title" : "GO Desde 0",
+      "image" : "https://edteam-media.s3.amazonaws.com/courses/original/91e149d0-961a-4594-a8ff-0a625be9cdd2.png",
+      "price" :  "120 USD" ,
+      "profesor": "Juan ordoñez"} , 
+    { "id":4,
+      "title" : "SEO Desde 0",
+      "image" : "https://edteam-media.s3.amazonaws.com/courses/original/9828742f-4da6-426e-80c9-e5081cc7f7a9.png",
+      "price" :  "20 USD" , 
+      "profesor": "Lucero Alberto"} , 
+  ]
 
-)
+const Course = ({match}) => {
+    /**Utilizo filter para que me filtre y me devuelva un array de 1 elemento */
+    const CursoActual = Cursos.filter ( c => c.id === parseInt(match.params.id))[0]
+    console.log("ver consola: ", CursoActual)
+    
+    return (
+       
+      <div className="ed-grid m-grid-3">
+        {CursoActual?(
+                <>  
+                    {/**<h1 className="m-cols-3">Curso: {match.params.id}</h1>*/}
+                    <h1 className="m-cols-3">{CursoActual.title}</h1>
+                    <img className="m-cols-1" src= {CursoActual.image} alt= {CursoActual.title}/>
+                    <p className="m-cols-2"> Profesor: {CursoActual.profesor} </p>
+                </> 
+            ) : ( 
+                <>  
+                    <div className="ed-grid">
+                    <h1>Error 404</h1>
+                    <span>Página no encontrada</span>
+                    </div>
+                </> 
+            )
+        }
+        {/**
+         * Otra forma de hacer el if ternario:
+         (() => {
+            if (CursoActual) {
+              return (
+                <>  
+                  <h1 className="m-cols-3">{CursoActual.title}</h1>
+                  <img className="m-cols-1" src= {CursoActual.image} alt= {CursoActual.title}/>
+                  <p className="m-cols-2"> Profesor: {CursoActual.profesor} </p>
+                </> 
+              )
+            } else if (CursoActual==null) {
+              return (
+                <>  
+                  <div className="ed-grid">
+                  <h1>Error 404</h1>
+                  <span>Página no encontrada</span>
+                  </div>
+                </> 
+              )
+            } else {
+              return (
+                <div>nada</div>
+              )
+            }
+          })()
+        */}
+      </div>
+    )
+}
 
 export default Course
